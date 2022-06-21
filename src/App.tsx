b96d1d5cb2047bf2d344eca3d2f5aa879169675c
SunlightLuck <sunlight.luck@gmail.com>
2022-06-21 15:33:00 -0400

    wallet-select-added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 
+import '@solana/wallet-adapter-react-ui/styles.css'
+
 function App() {
   return (
     <div className="App">
