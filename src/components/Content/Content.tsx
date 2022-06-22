edcadae2e57bce3fb32fbed4139f06ab53888f97
SunlightLuck <sunlight.luck@gmail.com>
2022-06-22 02:53:07 -0400

    send transaction

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,6 +1,7 @@
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base'
 import { useWallet, useConnection } from '@solana/wallet-adapter-react'
-import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js'
-import React, { useEffect, useState } from 'react'
+import { Connection, PublicKey, LAMPORTS_PER_SOL, TransactionSignature, Keypair, Transaction, SystemProgram } from '@solana/web3.js'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Content: React.FC = () => {
@@ -13,6 +14,42 @@ const Content: React.FC = () => {
     setBalance(balance / LAMPORTS_PER_SOL)
   }
 
+  const airDrop = useCallback(async () => {
+    if(!wallet.publicKey) {
+      console.log("Wallet not Connected");
+      return;
+    }
+    let signature: TransactionSignature = '';
+
+    try {
+      console.log("DROP")
+      signature = await connection.requestAirdrop(wallet.publicKey, 1 * LAMPORTS_PER_SOL);
+      await connection.confirmTransaction(signature, 'confirmed');
+    } catch(err) {
+      console.log(err)
+    }
+  }, [wallet.publicKey, connection]);
+
+  const sendTransaction = useCallback(async() => {
+    if(!wallet.publicKey) {
+      console.log('Wallet not Connected');
+      return;
+    }
+
+    const destAddress = Keypair.generate().publicKey;
+    const amount = .001 * LAMPORTS_PER_SOL
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: wallet.publicKey,
+        toPubkey: destAddress,
+        lamports: amount
+      })
+    );
+    const signature = await wallet.sendTransaction(transaction, connection);
+    await connection.confirmTransaction(signature, 'confirmed');
+  }, [wallet.publicKey, connection])
+
   useEffect(() => {
     if(wallet.publicKey) {
       getUserBalance(wallet.publicKey, connection)
@@ -22,8 +59,14 @@ const Content: React.FC = () => {
   return (
     <Container>
       <div>
+        <Button onClick={airDrop}>AIRDROP 1</Button>
+      </div>
+      <div>
         {wallet.publicKey && <p>Public Key: {wallet.publicKey.toBase58()}</p>}
         {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>}
+      </div>
+      <div>
+        {wallet.publicKey && <Button onClick={sendTransaction}>SEND TRANSACTION</Button>}
       </div>
     </Container>
   )
@@ -37,6 +80,20 @@ const Container = styled.div`
   > div {
     color: white;
     text-align: center;
+  }
+`
+
+const Button = styled.button`
+  padding: 15px 30px;
+  background-color: #666;
+  border: none;
+  color: white;
+  cursor: pointer;
+  :hover {
+    background-color: #444;
+  }
+  :active {
+    background-color: #555;
   }
 `
 
